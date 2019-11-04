81abc302fb3fb7881cb8161335e3b17a0af25e90
swarooph12 <56455125+swarooph12@users.noreply.github.com>
2019-11-05 01:59:49 +0530

    Create quotes_spider.py

---
+++ quotes_spider.py
@@ -0,0 +1,14 @@
+import scrapy
+class Quotesspider(scrapy.Spider):
+
+    def start_requests(self):
+        urls=['quotes.toscrape.com/page/1/','quotes.toscrape.com/page/2/']
+
+        for url in urls:
+            yield scrapy.Request(url=url,callback=self.parse)
+    def parse(self,response):
+        page=response.url.split('/')[-2]
+        filename='quotes-%s.html' %page
+        with open (filename,'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)    
